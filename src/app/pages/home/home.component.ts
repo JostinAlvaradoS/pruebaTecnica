8c21658a272d9b2436ea8980bc135d0f483d716d
chostim <jostinalvaradosarmiento@gmail.com>
2024-12-20 22:38:57 -0500

    loading e inicializacion del home agregado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,23 +1,17 @@
-import { Component } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  currentRoute: string = '';
+export class HomeComponent implements OnInit {
+  isLoading = true;
 
-  constructor(private router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.currentRoute = event.urlAfterRedirects;
-      }
-    });
-  }
-
-  isActive(route: string): boolean {
-    return this.currentRoute === route;
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+      console.log('Home component initialized');
+    }, 500); 
   }
 }
